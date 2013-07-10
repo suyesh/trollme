96f1022317021d0b3fec033f63e82248da8d46c7
suyeshb@gmail.com <suyeshb@gmail.com>
2013-07-10 00:03:38 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
+gem "paperclip", "~> 3.0"
+gem 'aws-sdk'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+
+group :production do
+	gem 'pg'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'ae123144d94145413203b2f5e23d57157b636f4e202216cbd36faddb653cacf47fe3e02c44e461550ba2ab1cbc16074e5c3d4fd5db1e3d545b6fe43b9655b23b'
